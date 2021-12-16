f46ab32038b35d1426681b9b79facaf9bafbe808
Konstantin Serykh <konstantin.seryh@gmail.com>
2021-12-16 12:26:24 +0400

    add reviews models

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from users.models import User
 
 CHANGE_USERNAME = 'Нельзя создать пользователя с username = "me"'
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users.apps.UsersConfig',
+    'reviews.apps.ReviewsConfig',
     'api'
 ]
 
